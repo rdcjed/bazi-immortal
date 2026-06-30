94769b31f72db8b049f437263c40079046d4fb56
rdcjed <rdcjed@gmail.com>
2026-06-30 20:58:10 +0800

    🔒 .env移出版本控制，添加gitignore

--- start_llm.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /d E:\AI相关\hermes\program\bazi-immortal
-set SENSENOVA_API_KEY=sk-YQXF3e87xb8zHZxcjwKOgYf2IaLJRmih
-set LLM_QUALITY_CHECK=true
-start "bazi-immortal" pythonw web\app.py
-echo 道士预测已启动！
-echo 访问地址: http://localhost:5000
-pause
